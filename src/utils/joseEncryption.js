c25190b6156a763c582cd3f449e7d884353fdd31
Mr-Rana-Coder <ranasatish297@gmail.com>
2025-02-15 14:48:49 +0530

    Encryption file updated

--- src/utils/joseEncryption.js
+++ src/utils/joseEncryption.js
@@ -1,54 +1,74 @@
 import jose from "node-jose";
+import { ApiError } from "./ApiError";
 
-const encryptPayloadAccessToken = async (payload) => {
-    const encKey = await jose.JWK.asKey(Buffer.from(process.env.ENC_KEY_ACCESS_TOKEN, "base64"), "oct");
-    if (!encKey) {
-        throw new ApiError(400, "Enc Key is required")
+const accessTokenGeneration = async (payload) => {
+    try {
+        const exp = Math.floor(Date.now() / 1000) + process.env.ACCESS_TOKEN_EXPIRY;
+        const payloadWithExp = { ...payload, exp };
+
+        const encKey = await jose.JWK.asKey(Buffer.from(process.env.ENC_KEY_ACCESS_TOKEN, "base64"), "oct");
+        if (!encKey) {
+            throw new ApiError(400, "Enc Key is required")
+        }
+        const encrypted = await jose.JWE.createEncrypt({ format: "compact" }, encKey)
+            .update(JSON.stringify(payloadWithExp))
+            .final();
+
+        if (!encrypted) {
+            throw new ApiError(500, "Payload encryption failed in access token")
+        }
+
+        const signKey = await jose.JWK.asKey(Buffer.from(process.env.SIGN_KEY_ACCESS_TOKEN, "base64"), "oct");
+        if (!signKey) {
+            throw new ApiError(400, "Sign Key is missing")
+        }
+
+        const signed = await jose.JWS.createSign({ format: "compact" }, signKey)
+            .update(encrypted)
+            .final();
+        return signed;
+
+    } catch (error) {
+        console.error("Unable to generate Access token", error.message);
+        throw new ApiError(500, "Access token generation failed");
     }
-    const encrypted = await jose.JWE.createEncrypt({ format: "compact" }, encKey)
-        .update(JSON.stringify(payload))
-        .final();
-    return encrypted;
+
 }
 
-const signPayloadAccessToken = async (encryptedData) => {
-    const signKey = await jose.JWK.asKey(Buffer.from(process.env.SIGN_KEY_ACCESS_TOKEN, "base64"), "oct");
-    if (!signKey) {
-        throw new ApiError(400, "Sign Key is missing")
-    }
-    const signed = await jose.JWS.createSign({ format: "compact" }, signKey)
-        .update(encryptedData)
-        .final();
+const refreshTokenGeneration = async (payload) => {
+    try {
+        const exp = Math.floor(Date.now() / 1000) + process.env.REFRESH_TOKEN_EXPIRY;
+        const payloadWithExp = { ...payload, exp };
 
-    return signed;
-};
+        const encKey = await jose.JWK.asKey(Buffer.from(process.env.ENC_KEY_REFRESH_TOKEN, "base64"), "oct");
+        if (!encKey) {
+            throw new ApiError(400, "Enc Key is required")
+        }
+        const encrypted = await jose.JWE.createEncrypt({ format: "compact" }, encKey)
+            .update(JSON.stringify(payloadWithExp))
+            .final();
 
-const encryptPayloadRefreshToken = async (payload) => {
-    const encKey = await jose.JWK.asKey(Buffer.from(process.env.ENC_KEY_REFRESH_TOKEN, "base64"), "oct");
-    if (!encKey) {
-        throw new ApiError(400, "Enc Key is required")
+        if (!encrypted) {
+            throw new ApiError(500, "Payload encryption failed in refresh token")
+        }
+
+        const signKey = await jose.JWK.asKey(Buffer.from(process.env.SIGN_KEY_REFRESH_TOKEN, "base64"), "oct");
+        if (!signKey) {
+            throw new ApiError(400, "Sign Key is missing")
+        }
+        const signed = await jose.JWS.createSign({ format: "compact" }, signKey)
+            .update(encrypted)
+            .final();
+
+        return signed;
+
+    } catch (error) {
+        console.error("Unable to generate Refresh token", error.message);
+        throw new ApiError(500, "Refresh token generation failed");
     }
-    const encrypted = await jose.JWE.createEncrypt({ format: "compact" }, encKey)
-        .update(JSON.stringify(payload))
-        .final();
-    return encrypted;
 }
-
-const signPayloadRefreshToken = async (encryptedData) => {
-    const signKey = await jose.JWK.asKey(Buffer.from(process.env.SIGN_KEY_REFRESH_TOKEN, "base64"), "oct");
-    if (!signKey) {
-        throw new ApiError(400, "Sign Key is missing")
-    }
-    const signed = await jose.JWS.createSign({ format: "compact" }, signKey)
-        .update(encryptedData)
-        .final();
-
-    return signed;
-};
 
 export {
-    encryptPayloadAccessToken,
-    encryptPayloadRefreshToken,
-    signPayloadAccessToken,
-    signPayloadRefreshToken
+    accessTokenGeneration,
+    refreshTokenGeneration
 }
